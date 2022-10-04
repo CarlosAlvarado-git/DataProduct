864804b779768c5d6aeefa07977c25c1d11a61c8
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2022-10-03 18:51:30 -0600

    lab2 en clase

---
+++ laboratorios/laboratorio2/parte_jsonfile_titanic.R
@@ -0,0 +1,64 @@
+library(plumber)
+library(lubridate)
+library(jsonlite)
+library(stringr)
+library(dplyr)
+library(rpart)
+#* @apiTitle Modelo del Titanic
+#* @apiDescription Este api nos servira para predicir
+#* si un pasajero del titanic sobrevive o no
+#* 
+
+#* Log some information about the incoming request
+#* @filter logger
+function(req){
+  largo <- length(req$args)
+  if(largo > 0){
+    fecha <- Sys.time()
+    path <- paste0(getwd(),'/year=', year(fecha),'/month=', month(fecha),'/day=', day(fecha),'/',hour(fecha))
+    path <- str_replace_all(path, fixed(" "), "")
+    if (file.exists(path)) {
+      
+      cat("The folder already exists")
+      
+    } else {
+      
+      dir.create(path, recursive = TRUE)
+      
+    }
+    
+    ListJSON <- toJSON(list('req'=req$args, 'query'=req$QUERY_STRING, 'user_agent'=req$HTTP_USER_AGENT),auto_unbox=TRUE)
+    write(ListJSON, file = paste0(path, '/', fecha, '.json'))
+    plumber::forward()
+    
+  }
+  plumber::forward()
+}
+
+fit <- readRDS("modelo_final.rds")
+
+#* Prediccion de sobrevivencia de un pasajero
+#* @param Pclass clase en el que viajabe el pasajero
+#* @param Sex Sexo del pasajero
+#* @param Age edad del pasajero
+#* @param SibSp numero de hermanos
+#* @param Parch numero de parientes
+#* @param Fare precio del boleto
+#* @param Embarked puerto del que embarco
+#* @post /titanic
+
+function(Pclass, Sex, Age, SibSp, Parch, Fare, Embarked){
+  features <- data_frame(Pclass = as.integer(Pclass),
+                         Sex,
+                         Age=as.integer(Age),
+                         SibSp= as.integer(SibSp),
+                         Parch = as.integer(Parch),
+                         Fare = as.numeric(Fare),
+                         Embarked)
+  out <- predict(fit,features,type = "class")
+  as.character(out)
+}
+
+# list('req'=req$args, 'query'=req$QUERY_STRING, 'user_agent'=req$HTTP_USER_AGENT)
+
+#/end_point/year=XXX/month=XX/day=XX/hour=XX
